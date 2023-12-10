65493ea205a4635a0b264e57181795261e413424
Itay Gabzo <itaygabzo11@gmail.com>
2023-12-10 14:23:15 +0200

    finished addition

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+# author : Itay Gabzo
+
+
+def addition(num1,num2):
+    if(type(num1) is int or type(num1) is double):
+        if(type(num2) is int or type(num2) is double):
+            print(num1+num2)
+        else:
+            print("Input error")
+    else:
+        print("Input error")
+
+
+def subtraction(num1, num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def multification(num1,num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def division(num1,num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def power(num1,num2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
